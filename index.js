e7d831e46ac25165172f5538b3e24ee8b945f274
nicolaskobelt <kobeltnicolas@me.com>
2016-04-25 18:57:38 -0300

    Division de todas las paginas

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by NicolasKobelt on 25/4/16.
+ */
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="boton1" onclick="comoFunciona()">¿Como funciona?</p>
+<script>
+    function comoFunciona() {
+    document.getElementById("demo").style.color = "red";
