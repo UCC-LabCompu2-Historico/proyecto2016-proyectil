27174170e51a04fd67d361ff10041791ef91adb1
nik <kobeltnicolas@me.com>
2016-06-02 09:32:22 -0300

    Comenzando con js

---
+++ formulario.js
@@ -0,0 +1,9 @@
+/**
+ * Created by AlumnoXP on 02/06/16.
+ */
+
+var formularioPrincipal = document.formulario;
+    var primerElemento = document.formulario.input1;
+    var primerElemento = document.formulario.input2;
+    var primerElemento = document.formulario.input3;
+    var primerElemento = document.formulario.input4;
