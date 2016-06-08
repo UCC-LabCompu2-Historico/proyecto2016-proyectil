4b75fa8c85caeb32d5092c1376cd0c0aeae217d7
nicolaskobelt <kobeltnicolas@me.com>
2016-06-08 11:54:47 -0300

    No cazo una de js

--- formulario.js
+++ formulario.js
@@ -4,6 +4,15 @@
 
 var formularioPrincipal = document.formulario;
     var primerElemento = document.formulario.input1;
-    var primerElemento = document.formulario.input2;
-    var primerElemento = document.formulario.input3;
-    var primerElemento = document.formulario.input4;
+    var segundoElemento = document.formulario.input2;
+    var tercerElemento = document.formulario.input3;
+    var cuartoElemento = document.formulario.input4;
+    
+
+function sigPagina (nombre) {
+
+    if (nombre == "Dtotal") {
+        document.getElementById('formulario.html')
+    }
+
+}
