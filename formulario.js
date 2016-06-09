bd0a7a4bcc6a2c22ff27a4d38eb66cf2bd3f8047
Nik <kobeltnicolas@me.com>
2016-06-09 09:45:50 -0300

    Hasta las huevo

--- formulario.js
+++ formulario.js
@@ -2,17 +2,46 @@
  * Created by AlumnoXP on 02/06/16.
  */
 
+
+//parte de selector.html //
+
+function sigPagina (nombre) {
+
+    if (nombre == "Dtotal") {
+        window.location.replace('formulario.html');
+    }
+
+    else if (nombre == "Ang"){
+        window.location.replace('formulario.html');
+    }
+
+    else if (nombre == "Vinicial"){
+        window.location.replace('formulario.html');
+    }
+
+    else if (nombre == "Alt"){
+        window.location.replace('formulario.html');
+    }
+}
+
+//parte de formulario.html //
+
+
 var formularioPrincipal = document.formulario;
     var primerElemento = document.formulario.input1;
     var segundoElemento = document.formulario.input2;
     var tercerElemento = document.formulario.input3;
     var cuartoElemento = document.formulario.input4;
-    
 
-function sigPagina (nombre) {
 
-    if (nombre == "Dtotal") {
-        document.getElementById('formulario.html')
-    }
 
+// resultado //
+
+function resultadoVelocidad() {
+    var el = document.getElementById("formulario").elements;
+
+    var url = 'resultado.html#' + el["masa"].value + "#" + el["unidadesMasa"].value + "#" + el["altura"].value;
+    window.location.replace('resultado.html');
 }
+
+
