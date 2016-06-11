eda95084bd95679299b817fc508ed8f95e0aa270
nicolaskobelt <kobeltnicolas@me.com>
2016-06-11 19:22:18 -0300

    Error en js

--- formulario.js
+++ formulario.js
@@ -1,18 +1,43 @@
-/**
- * Created by AlumnoXP on 02/06/16.
- */
 
-var formularioPrincipal = document.formulario;
-    var primerElemento = document.formulario.input1;
-    var segundoElemento = document.formulario.input2;
-    var tercerElemento = document.formulario.input3;
-    var cuartoElemento = document.formulario.input4;
-    
+
+//parte de selector.html //
 
 function sigPagina (nombre) {
 
     if (nombre == "Dtotal") {
-        document.getElementById('formulario.html')
+        window.location.replace('formularioDistancia.html');
     }
 
+    else if (nombre == "Ang") {
+        window.location.replace('formulario.html');
+    }
+
+    else if (nombre == "Vinicial") {
+        window.location.replace('formulario.html');
+    }
+
+    else if (nombre == "Alt") {
+        window.location.replace('formulario.html');
+    }
+}
+
+// formularioDistancia  //
+
+    function valDistacia () {
+        var el=document.getElementById("formDist").elements;
+        var url = 'resultadoDistancia.html#' + "#" + el["vInicial"].value + "#" + el["aSalida"].value;
+        window.location.replace("resultadoDistancia");
+        
+    }
+
+
+
+
+// resultado //
+
+function resultadoVelocidad() {
+    var el = document.getElementById("formulario").elements;
+
+    var url = 'resultado.html#' + el["masa"].value + "#" + el["unidadesMasa"].value + "#" + el["altura"].value;
+    window.location.replace('resultado.html');
 }
