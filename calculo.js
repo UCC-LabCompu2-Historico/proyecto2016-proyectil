939148b8420b5b78b5a75321f388ee19102f7cbd
nicolaskobelt <kobeltnicolas@me.com>
2016-12-07 10:53:50 -0300

    Comienzo de javascript

--- calculo.js
+++ calculo.js
@@ -7,14 +7,14 @@ function calc() {
     var angulo = document.getElementById('angulo').value;
     var tiempo = document.getElementById('tiempo').value;
 
-    var anguloRad = (angulo/180)*Math.PI;
+    var anguloRad = (angulo/180)*Math.PI; //Pase el angulo a radianes
 
-    var altmax = ((velocidad**2)*(Math.sin(anguloRad)))/(2*9.8);
+    var altmax = ((velocidad**2)*(Math.sin(anguloRad)))/(2*9.8); //calculo de la altura maxima del objeto
+    var altmaxS = altmax.toFixed(3); //RESULTADO FINAL truncado a 3 cifras
 
+    var alcanMax = (velocidad**2)/9.8; //calculo de alcance maximo en x
+    var alcanMaxF = alcanMax.toFixed(3); // RESULTADO FINAL truncado a 3 cifras
 
-    var alcanMax = (velocidad**2)/9.8;
-    var alcanMaxF = alcanMax.toFixed(3);
-
-
+    var eqTrayect = (Math.tan(anguloRad)*)
 }
 
