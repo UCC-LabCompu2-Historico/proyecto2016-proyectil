91d37a0c8340731afd09b93ca9c019919da1e883
nicolaskobelt <kobeltnicolas@me.com>
2016-12-13 12:05:56 -0300

    13/12

--- canvas.js
+++ canvas.js
@@ -2,8 +2,26 @@
  * Created by nicolaskobelt on 12/12/16.
  */
 
-function myCanvas() {
 
-    
-    
-}
+//funcion para tomar valores ingresados
+function miDibujo() {
+
+    var myCanvas = document.getElementById("canvas");
+    var ctx = myCanvas.getContext("2d");
+    var X = alcanMax;
+    var Y = altMax;
+
+    }
+//intento de animacion 
+    function mAnimar (){
+
+        myCanvas.width= myCanvas.width;
+        ctx.beginPath();
+        ctx.arc(X,Y,0,2*Math.PI);
+        ctx.moveTo(X,0);
+        ctx.lineTo(X,Y);
+        ctx.fillStyle = 'black';
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
